b8017590c04d0c637cace7777f5769c96512134d
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-09 09:59:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
+  function dateDiffInDays(date1, date2) {
+  const oneDay = 24 * 60 * 60 * 1000; // One day in milliseconds
+  const firstDate = Date.UTC(
+    parseInt(date1.substring(0, 4)), // Year
+    parseInt(date1.substring(5, 7)) - 1, // Month (zero-based)
+    parseInt(date1.substring(8, 10)) // Day
+  );
+
+  const secondDate = Date.UTC(
+    parseInt(date2.substring(0, 4)), // Year
+    parseInt(date2.substring(5, 7)) - 1, // Month (zero-based)
+    parseInt(date2.substring(8, 10)) // Day
+  );
+
+  const diffInMs = secondDate - firstDate;
+  return Math.round(diffInMs / oneDay);
+}
+
 };
 
 // Do not change the code below.
