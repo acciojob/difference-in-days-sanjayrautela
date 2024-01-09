1db2eb99fa128bb849aacbba1c8ce521cd464dd7
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-09 10:02:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var dateDiffInDays = function (date1, date2) {
-  function dateDiffInDays(date1, date2) {
   const oneDay = 24 * 60 * 60 * 1000; // One day in milliseconds
   const firstDate = Date.UTC(
     parseInt(date1.substring(0, 4)), // Year
@@ -15,11 +14,9 @@ var dateDiffInDays = function (date1, date2) {
 
   const diffInMs = secondDate - firstDate;
   return Math.round(diffInMs / oneDay);
-}
-
 };
 
 // Do not change the code below.
-const dateOne = prompt("Enter Start Date.");
-const dateTwo = prompt("Enter End Date.");
+const dateOne = prompt("Enter Start Date (YYYY-MM-DD):");
+const dateTwo = prompt("Enter End Date (YYYY-MM-DD):");
 alert(dateDiffInDays(dateOne, dateTwo));
